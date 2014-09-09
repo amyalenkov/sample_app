96ea61e6b79eab2e1782a3a90a5010053b39b2b7
amyalenkov <amyalenkov@intervale.ru>
2014-09-09 17:16:57 +0300

    all users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
   def new
     @user = User.new
   end
@@ -8,8 +13,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params.require(:user).permit(:name, :email, :password,
-                                                   :password_confirmation))
+    @user = User.new(user_params)
     if @user.save
       sing_in @user
       flash[:success] = "Welcome to the Sample App!"
@@ -17,6 +21,34 @@ class UsersController < ApplicationController
     else
       render 'new'
     end
+  end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def singed_in_user
+    redirect_to signin_url, notice: "Please sing in" unless sign_in?
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to (user_url) unless current_user?(@user)
   end
 end
